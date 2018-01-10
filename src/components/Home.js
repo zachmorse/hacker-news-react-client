255942ff9cf90ae185fbffc9231098d8b801a3a4
Zach Morse <zmorse@Zach-Morse-MBP.local>
2018-01-10 12:29:41 -0600

    integrated Open-Graph pictures into project

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,62 +1,42 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 // import Display from "./Display";
 
 export default class Home extends Component {
   state = {
-    storyIds: [],
     storyList: [],
     storyPageSize: 30,
-    pageNumber: 1
+    pageNumber: 1,
+    dataLoaded: false
+  };
+
+  getOgImageData = data => {
+    axios
+      .post("http://localhost:6060/get-og-images", data)
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          storyList: response.data,
+          dataLoaded: true
+        });
+        return response.data;
+      })
+      .catch(error => {
+        console.log("error", error);
+      });
   };
 
   async componentDidMount() {
     const newStoriesUrl = "http://node-hnapi.herokuapp.com/news";
     const response = await fetch(newStoriesUrl);
-    const storyList = await response.json();
-    this.setState({
-      storyList: storyList
-    });
-    console.log(this.state);
+    const stories = await response.json();
+    await this.getOgImageData(stories);
   }
-
-  // async componentDidMount() {
-  //   const newStoriesUrl =
-  //     "https://hacker-news.firebaseio.com/v0/newstories.json";
-  //   const itemBaseUrl = "https://hacker-news.firebaseio.com/v0/item/";
-  //   const response = await fetch(newStoriesUrl);
-  //   const storyIds = await response.json();
-  //   // const requests = storyIds.slice(0, this.state.storyPageSize).map(id => {
-  //   //   return fetch(itemBaseUrl.concat(`${id}${".json"}`));
-  //   // });
-
-  //   // getting all new stories from the server
-  //   const requests = storyIds.map(id => {
-  //     return fetch(itemBaseUrl.concat(`${id}${".json"}`));
-  //   });
-
-  //   const results = await Promise.all(requests);
-  //   const resultsJSON = results.map(result => result.json());
-  //   const storyItems = await Promise.all(resultsJSON);
-
-  //   // filtering out text requests, to leave only links
-  //   const filteredStories = await storyItems.filter(
-  //     storyItems => storyItems.url
-  //   );
-  //   console.log(filteredStories);
-
-  //   this.setState({
-  //     storyItems: filteredStories,
-  //     storyIds: storyIds
-  //   });
-
-  //   console.log(this.state);
-  // }
 
   render() {
     const { storyList } = this.state;
 
-    if (storyList.length < this.state.storyPageSize) {
+    if (this.state.dataLoaded === false) {
       return <div>Loading...</div>;
     }
 
@@ -67,10 +47,22 @@ export default class Home extends Component {
           id={index}
           className="column is-4 has-text-centered column-width-adjust"
         >
-          <a href={details.url} target="blank">
-            <p className="title is-size-5">{details.title} </p>
-          </a>
-          <span className="is-size-7">({details.domain})</span>
+          <div className="fadeUp">
+            <figure className="image-container">
+              <a href={details.url} target="blank">
+                <img
+                  src={details.ogImage}
+                  className="image-adjustment"
+                  alt={details.title}
+                />
+              </a>
+            </figure>
+            <hr />
+            <a href={details.url} target="blank">
+              <p className="title is-size-5">{details.title} </p>
+            </a>
+            <span className="is-size-7">({details.domain})</span>
+          </div>
         </div>
       );
     });
